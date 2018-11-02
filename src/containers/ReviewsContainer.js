5f8e386adb932649934c49908ed3d292d665ea51
derrick b <dbozich1@gmail.com>
2018-11-02 22:56:16 +0000

    automatically backed up by learn

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,29 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import ReviewInput from '../components/reviews/ReviewInput'
 import Reviews from '../components/reviews/Reviews'
 import { connect } from 'react-redux'
 
 class ReviewsContainer extends Component {
-
   render() {
-
-  const reviews = this.props.reviews.filter(r => r.restaurantId === this.props.restaurant.id)
     return (
       <div>
-        <ReviewInput addReview={this.props.addReview} restaurant={this.props.restaurant} />
-        <Reviews reviews={reviews} deleteReview={this.props.deleteReview} />
+        <ReviewInput
+          addReview={this.props.addReview}
+          restaurantId={this.props.restaurant.id}
+        />
+        <Reviews
+          reviews={this.props.reviews}
+          restaurantId={this.props.restaurant.id}
+          deleteReview={this.props.deleteReview}
+        />
       </div>
-    )
+    );
   }
 }
 
+const mapStateToProps = ({reviews}) => {
+  return {reviews}
+}
+
 const mapDispatchToProps = dispatch => ({
-  addReview: (text, restaurantId) => dispatch({type: 'ADD_REVIEW', payload: {text, restaurantId}}),
-  deleteReview: reviewId => dispatch({type: 'DELETE_REVIEW', payload: reviewId})
+  addReview: review => dispatch({type: 'ADD_REVIEW', review}),
+  deleteReview: id => dispatch({type: 'DELETE_REVIEW', id})
 })
-
-const mapStateToProps = ({reviews}) => ({reviews})
-
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(ReviewsContainer)
