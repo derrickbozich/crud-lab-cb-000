3e12cfc01b4a0c2e3f4242719be038d5e6dddd88
derrick b <dbozich1@gmail.com>
2018-11-02 22:54:30 +0000

    automatically backed up by learn

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -1,4 +1,18 @@
+import React, { Component } from 'react';
 
+class ReviewInput extends Component {
+
+  state = {
+    text: ''
+  }
+
+  handleOnChange = event => {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleOnSubmit = event => {
     event.preventDefault();
     this.props.addReview({text: this.state.text, restaurantId: this.props.restaurantId });
     this.setState({
